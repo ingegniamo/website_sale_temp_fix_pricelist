33c70b91fd41358b0bda6e622378d6d3b9251dd0
micheledic <dicroce.m@stesi.eu>
2023-09-05 11:49:39 +0200

    Initial commit

---
+++ __manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "",
+    
+    'summary': "",
+  
+    'license': 'OPL-1',
+
+    'author': "STeSI Consulting",
+
+    'category': '',
+  
+    'version': '16.0.0.1',
+  
+    'website': "http://www.stesi.consulting",
+
+    # any module necessary for this one to work correctly
+    'depends': [],
+    
+    # always loaded
+    'data': [],
+
+    'application': False,
+}
